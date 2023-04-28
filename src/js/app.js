02035d5ac7f01ded02bc7d6758af6569b3493d79
williamjrr07 <114874231+williamjrr07@users.noreply.github.com>
2023-04-27 23:21:06 -0400

    talento digital

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// HABILITAR COMPONENTES DE BOOTSTRAP
+// tooltip
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]',
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl),
+);
+
